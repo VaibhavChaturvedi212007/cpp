a78e6b2bc4eef4e87376db46e33917d70e99e025
VaibhavChaturvedi <chitranshchaturvedi21052007@gmail.com>
2025-11-01 23:52:29 +0530

    CONDITIONAL STATEMENT USING if, else and else if

---
+++ CONDITIONSTATEMENT1.CPP
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main (){
+    // using if statement
+    // int x=20;          
+    //  int y=18;
+    //  if(x>y);{
+    //     cout<<"x is greater than y";
+    //  }
+   
+   
+    // // using of else statement
+    // int x=20;
+    // if (x<18){
+    //     cout<<"good evening";
+    // } else{
+    //     cout<<"good morning";
+    // }
+
+    // using of else if statement
+    int x=22;
+    if(x<10) {
+        cout<<"good morning";
+    } else if (x<20){
+        cout<<"good day";
+    } else{
+        cout<<"good evening";
+    }
+    
+}
