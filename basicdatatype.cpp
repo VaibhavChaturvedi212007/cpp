0f5962b80ce0d549e73a8eb9a002c9399075c90e
VaibhavChaturvedi <chitranshchaturvedi21052007@gmail.com>
2025-11-01 17:49:25 +0000

    basic.cpp

---
+++ basicdatatype.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int age=25;
+    // cout<<age<<endl;
+    // char grade ='A';
+    // cout<<grade;
+    // float PI=3.14f;
+    // cout<<PI;
+    // bool isSafe=false;
+    // bool isSafe=true;
+    // cout<<isSafe;
+    // double price=100.99;
+    // cout<<price;
+    int age=25;
+    cout<<sizeof(age);
+    return 0;
+}
