61e3eb68f273289a57ef38e7aacf740df59215b5
VaibhavChaturvedi <chitranshchaturvedi21052007@gmail.com>
2025-11-06 21:30:52 +0530

    using more operation conditional statements

---
+++ conditionalstatement2.cpp
@@ -0,0 +1,89 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int time=20;
+//     cout<<((time<18)?"good":"night");
+//     return 0;
+
+// }
+
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// int main(){
+//     int time=22;
+//     cout<<((time<18)?"good morning":(time==18)?"good":"night");
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x,y;
+//     cin>>x>>y;
+//     if (x>12){
+//     cout<<"x is greater"<<endl;
+//     if (y>10){
+//         cout<<"y is greter than"<<endl;
+//     }
+//     }else{
+//         cout<<"no one is greater";
+//     }
+//     return 0;
+    
+    
+// }
+// #include<iostream>
+// using namespace std;
+// int main (){
+//     int age;
+//     cin>>age;
+//     bool iscitzen=true;
+//     if (age>18){
+//         cout<<"you are eligible to vote"<<endl;
+//         if(iscitzen){
+//             cout<<"you are citzen to vote"<<endl;
+//         }else{
+//             cout<<"you are not a citzen to vote";
+//         }
+//         }else{
+//             cout<<"you are not eligible to vote"<<endl;
+//     }
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main (){
+//     int a,b,c;
+//     cin>>a>>b>>c;
+//     if(a>b&&b>c){
+//         cout<<"both the condition are true";
+//     }else{
+//         cout<<"both condition are not true";
+//     }
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,s,d;
+//     cin>>a>>s>>d;
+//     if (a>s||s<d){
+//         cout<<"at least one condition is true"<<endl;
+//     }
+    
+//     return 0;
+// }
+ #include<iostream>
+ using namespace std;
+ int main (){
+     int a,b;
+     cin>>a>>b;
+     if(!(a>b)){
+         cout<<"a is not greater than b"<<endl;
+        
+     }else{
+         cout<<"a is greater than b";
+     }
+     return 0;
+ }
