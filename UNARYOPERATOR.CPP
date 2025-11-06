93c2dcd893e0cf494b1e8207693ff757c4b2effb
VaibhavChaturvedi <chitranshchaturvedi21052007@gmail.com>
2025-11-06 21:39:40 +0530

    USING :unary operator
    Demonstrate the use of unary operators in C++.

---
+++ UNARYOPERATOR.CPP
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a=10;
+	
+// 	int b=a++;
+// 	cout<<"b= "<<b<<endl;
+// 	cout<<"a= "<<a<<endl;
+   
+   
+    // int b=++a;
+    // cout<<"b= "<<b<<endl;
+    // cout<<"a= "<<a<<endl;
+    
+//     int b=--a;
+//     cout<<"b= "<<b<<endl;
+// 	cout<<"a= "<<a<<endl;
+
+
+    int b=a--;
+    cout<<"b= "<<b<<endl;
+	cout<<"a= "<<a<<endl;
+
+
+}
